3a5f9f0a958f0e484e4cb2d842fdbb34247270a7
papi <20916260+papi-ux@users.noreply.github.com>
2026-07-24 12:14:31 -0400

    test(nova): import JUnit boolean assertions in quick menu policy test

--- app/src/test/java/com/papi/nova/utils/DualScreenQuickMenuPolicyTest.kt
+++ app/src/test/java/com/papi/nova/utils/DualScreenQuickMenuPolicyTest.kt
@@ -1,6 +1,8 @@
 package com.papi.nova.utils
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class DualScreenQuickMenuPolicyTest {
