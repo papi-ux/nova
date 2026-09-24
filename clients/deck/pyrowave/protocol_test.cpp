df60a1ad49e091a8b68eb47f17f6202eae7fec8e
papi <michael@papi.miami>
2026-09-23 23:14:09 -0400

    test(linux): reject conflicting PyroWave payload mappings

--- clients/deck/pyrowave/protocol_test.cpp
+++ clients/deck/pyrowave/protocol_test.cpp
@@ -17,6 +17,7 @@ int main() {
                            map + "\na=fmtp:98 " NOVA_PYROWAVE_PROFILE_TOKEN "\n",
                            map + "\n" + profile + "\n" + profile,
                            map + "\n" + profile + "\na=fmtp:99 incompatible",
+                           map + "\n" + profile + "\na=rtpmap:99 H264/90000",
                            map + "\n" + profile + "\n" + map})
         require(!nova_pyrowave_description_compatible(bad.c_str()));
     std::cout << "PyroWave SDP exact profile, line endings and ambiguity rejection passed\n";
